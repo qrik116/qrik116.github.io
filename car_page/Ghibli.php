80b3cc77be9efe8bccd49084360a659c5688b16b
qrik116 <qrik116@gmail.com>
2016-11-11 16:19:24 +0300

    v.1.1

---
+++ car_page/Ghibli.php
@@ -0,0 +1,27 @@
+<div class="col-lg-9 col-sm-9">
+    <div class="row">
+        <div class="col-lg-12 col-sm-12">
+            <div class="main_title">
+                <h1><?php the_title();?></h1>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-lg-12 col-sm-12">
+            <div class="carousel">
+                <div class="big_pic">
+                    <img class="lazyOwl" data-src="<?php bloginfo('template_url')?>/images/Ghibli-1.jpeg" alt="Превью" />
+                </div>
+                <div class="big_pic">
+                    <img class="lazyOwl" data-src="<?php bloginfo('template_url')?>/images/Ghibli-2.jpeg" alt="Превью" />
+                </div>
+                <div class="big_pic">
+                    <img class="lazyOwl" data-src="<?php bloginfo('template_url')?>/images/Ghibli-3.jpeg" alt="Превью" />
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+</div>
---
+++ car_page/GranTurismo.php
@@ -0,0 +1,27 @@
+<div class="col-lg-9 col-sm-9">
+    <div class="row">
+        <div class="col-lg-12 col-sm-12">
+            <div class="main_title">
+                <h1>GranTurismo</h1>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-lg-12 col-sm-12">
+            <div class="carousel">
+                <div class="big_pic">
+                    <img class="lazyOwl" data-src="<?php bloginfo('template_url')?>/images/GranTurismo-1.jpeg" alt="Превью" />
+                </div>
+                <div class="big_pic">
+                    <img class="lazyOwl" data-src="<?php bloginfo('template_url')?>/images/GranTurismo-2.jpeg" alt="Превью" />
+                </div>
+                <div class="big_pic">
+                    <img class="lazyOwl" data-src="<?php bloginfo('template_url')?>/images/GranTurismo-3.jpeg" alt="Превью" />
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+</div>
